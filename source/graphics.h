94149a1bd4f732f305a2d14fbf05e6dc04759b6a
eric-pc-ssd <ericdesmet@outlook.com>
2021-05-29 11:05:05 -0400

    relative paths

--- source/graphics.h
+++ source/graphics.h
@@ -15,21 +15,21 @@ struct GL_State
     m4 perspectiveMatrix;
 };
 
+global char* PATH_VS_1 = "../source/shaders/SimpleVS.vert";
+global char* PATH_FS_1 = "../source/shaders/PositionFS.frag";
 
-global char* PATH_VS_1 = "C:/_Eric/Code/d3d/stormwarden/source/shaders/SimpleVS.vert";
-global char* PATH_FS_1 = "C:/_Eric/Code/d3d/stormwarden/source/shaders/PositionFS.frag";
+global char* PATH_VS_2 = "../source/shaders/2_PositionVS.vert";
+global char* PATH_FS_2 = "../source/shaders/2_PositionFS.frag";
 
-global char* PATH_VS_2 = "C:/_Eric/Code/d3d/stormwarden/source/shaders/2_PositionVS.vert";
-global char* PATH_FS_2 = "C:/_Eric/Code/d3d/stormwarden/source/shaders/2_PositionFS.frag";
+global char* PATH_VS_3 = "../source/shaders/3_PositionOffset.vert";
+global char* PATH_VS_3_CALC = "../source/shaders/3_CalcOffset.vert";
+global char* PATH_FS_3_CALC = "../source/shaders/3_CalcColor.frag";
 
-global char* PATH_VS_3 = "C:/_Eric/Code/d3d/stormwarden/source/shaders/3_PositionOffset.vert";
-global char* PATH_VS_3_CALC = "C:/_Eric/Code/d3d/stormwarden/source/shaders/3_CalcOffset.vert";
-global char* PATH_FS_3_CALC = "C:/_Eric/Code/d3d/stormwarden/source/shaders/3_CalcColor.frag";
+global char* PATH_VS_4_ORTHO_CUBE = "../source/shaders/4_OrthoCube.vert";
+global char* PATH_VS_4_MANUAL_PERSPECTIVE = "../source/shaders/4_ManualPerspective.vert";
 
-global char* PATH_VS_4_ORTHO_CUBE = "C:/_Eric/Code/d3d/stormwarden/source/shaders/4_OrthoCube.vert";
-global char* PATH_VS_4_MANUAL_PERSPECTIVE = "C:/_Eric/Code/d3d/stormwarden/source/shaders/4_ManualPerspective.vert";
-global char* PATH_VS_4_MATRIX_PERSPECTIVE = "C:/_Eric/Code/d3d/stormwarden/source/shaders/4_MatrixPerspective.vert";
-global char* PATH_FS_4_ORTHO_CUBE = "C:/_Eric/Code/d3d/stormwarden/source/shaders/4_OrthoCube.frag";
+global char* PATH_VS_4_MATRIX_PERSPECTIVE = "../source/shaders/4_MatrixPerspective.vert";
+global char* PATH_FS_4_ORTHO_CUBE = "../source/shaders/4_OrthoCube.frag";
 
 global GL_State gls_;
 global GL_State* GLS; 
